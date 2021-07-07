37a5ea49d6f49c90dc3af7f8bf3bb101d567dc3a
Hayden Carroll <haydencarroll77@gmail.com>
2021-07-06 20:44:27 -0700

    changed gitignore + some stuff

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -87,11 +87,16 @@ export default function() {
                     </div>
                 </div>
             
-                <div className="todo-section">
-                    <h1>// TODO</h1>
+                <div className="current-sprint-section">
+                    <h1>Current Sprint</h1>
 
-                    
-                </div>
+                    <ul>
+                        <li>IdahoCodes - Mentoring students learning to code at North Idaho College</li>
+                        <li>Learning React.js</li>
+                        <li>Messing around with 3D printer firmware</li>
+                        <li>Hanging out with friends and enjoying summer!</li>
+                    </ul>
+                  </div>
             </div>
             
         </div>
